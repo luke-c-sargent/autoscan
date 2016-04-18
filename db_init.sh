bb0bab670e62166591e98d08a5550086e950fec0
root <root@localhost.localdomain>
2016-04-18 15:05:16 -0400

    added network interface detection, changed echo to printf for portability/formatting

--- db_init.sh
+++ db_init.sh
@@ -1,13 +1,56 @@
 #!/bin/bash
-
 clear
 
-echo "checking database..."
+# function for quitting in error cases
+function quit {
+    printf "\n\t(╯°□°)╯︵ ┻━┻                 \n"
+    printf "  ...hope this wasn't during the presentation...\n\n"
+    exit
+}
+
+
+BANNER="
+ █████╗ ██╗   ██╗████████╗ ██████╗ ███████╗ ██████╗ █████╗ ███╗   ██╗
+██╔══██╗██║   ██║╚══██╔══╝██╔═══██╗██╔════╝██╔════╝██╔══██╗████╗  ██║
+███████║██║   ██║   ██║   ██║   ██║███████╗██║     ███████║██╔██╗ ██║
+██╔══██║██║   ██║   ██║   ██║   ██║╚════██║██║     ██╔══██║██║╚██╗██║
+██║  ██║╚██████╔╝   ██║   ╚██████╔╝███████║╚██████╗██║  ██║██║ ╚████║
+╚═╝  ╚═╝ ╚═════╝    ╚═╝    ╚═════╝ ╚══════╝ ╚═════╝╚═╝  ╚═╝╚═╝  ╚═══╝
+                                                                     
+"
+printf "$BANNER"
+#temporary title banner, for funsies
+
+# detect active network interface
+NIC=$(ip link show | grep 'state UP' | awk -F ': |:' '{print $2}')
+if [[ "$NIC" == "" ]]
+then
+    echo "No active connections"
+    quit
+fi
+
+#acquire and print out connection information
+printf "Network Connection:\n"
+CONNECT_INFO=$(iwconfig $NIC)
+ESSID=$(echo $CONNECT_INFO | grep "ESSID" | awk -F ':"|"' '{print $2}')
+MAC=$(echo $CONNECT_INFO | grep "Access Point" | awk -F ": | Bit" '{print $2}')
+printf "+[$NIC]:\tESSID: $ESSID ($MAC)\n"
+
+ifconfig $NIC down
+macchanger -r $NIC
+ifconfig $NIC up
+
+# checking on status of postgresql service
+printf "checking database...\n"
 CHECK=$(service postgresql status | grep dead)
 if [[ "$CHECK" == "   Active: inactive (dead)" ]]; then
-    echo "database inactive; enabling"
+    printf "database inactive; enabling\n"
     service postgresql start
 fi
+
+# database initialization
 msfdb init
 msfdb start
-msfconsole -r msfc_footprint.rc
+
+#launch metasploit with script file -- not yet implemented
+#msfconsole -r msfc_footprint.rc
